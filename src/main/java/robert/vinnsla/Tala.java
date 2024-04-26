95c641f2403ef644abd470eb8dafbe3430485c27
Róbert A. Jack <robertalex00@hotmail.com>
2024-04-26 16:16:07 +0000

    vinnslu klassinn tala gerður og einfaldar aðferir forrirtaðar

--- src/main/java/robert/vinnsla/Tala.java
+++ src/main/java/robert/vinnsla/Tala.java
@@ -1,7 +1,34 @@
-package robert.vinnsla;/******************************************************************************
+package robert.vinnsla;
+
+/******************************************************************************
  * @author Róbert A. Jack
  * Tölvupóstur: ral9@hi.is
- * Lýsing : 
+ * Lýsing : Vinnslu klasi fyrir reiknivélar forritið mitt. Skilgreinir tölu og
+ * ýmsar reikniaðferðir.
  *
- *****************************************************************************/public class tala {
+ *****************************************************************************/
+public class Tala {
+    public int tala;
+
+    public Tala(int tala){
+        this.tala=tala;
+    }
+    public Tala plus(Tala a, Tala b){
+        int samlagning = a.getTala() + b.getTala();
+        return new Tala(samlagning);
+    }
+    public Tala minus(Tala a, Tala b){
+        int fradrattur = a.getTala() - b.getTala();
+        return new Tala(fradrattur);
+    }
+    public Tala margfoldun(Tala a, Tala b){
+        int margfoldun = a.getTala() * b.getTala();
+        return new Tala(margfoldun);
+    }
+    public int getTala() {
+        return tala;
+    }
+    public void setTala(int a){
+        this.tala = a;
+    }
 }
